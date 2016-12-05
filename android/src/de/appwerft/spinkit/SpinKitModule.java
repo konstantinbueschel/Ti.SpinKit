2c73bcb74b4db31f99c6b6798efbb5a55e0236d0
Konstantin Büschel <konstantin@die-interaktiven.de>
2016-12-05 20:39:58 +0100

    * removed unnecessary build files * added build dir to git ignore * fixed module constants statement, only first constant was exposed * bumped version to 2.0.1

--- android/src/de/appwerft/spinkit/SpinKitModule.java
+++ android/src/de/appwerft/spinkit/SpinKitModule.java
@@ -8,24 +8,26 @@ import org.appcelerator.kroll.common.Log;
 
 @Kroll.module(name = "SpinKit", id = "de.appwerft.spinkit")
 public class SpinKitModule extends KrollModule {
+	
 	private static final String LCAT = "SpinModule";
 	
-	@Kroll.constant
-	public static final int TYPE_ROTATINGPLANE = 0;
-	public static final int TYPE_DOUBLEBOUNCE = 1;
-	public static final int TYPE_WAVE = 2;
-	public static final int TYPE_WANDERINGCUBES = 3;
-	public static final int TYPE_PULSE = 4;
-	public static final int TYPE_CHASINGDOTS = 5;
-	public static final int TYPE_THREEBOUNCE = 6;
-	public static final int TYPE_CIRCLE = 7;
-	public static final int TYPE_CUBEGRID = 8;
-	public static final int TYPE_FADINGCIRCLE = 9;
-	public static final int TYPE_FOLDINGCUBE = 10;
-	public static final int TYPE_ROTATINGCIRCLE = 11;
+	@Kroll.constant public static final int TYPE_ROTATINGPLANE = 0;
+	@Kroll.constant public static final int TYPE_DOUBLEBOUNCE = 1;
+	@Kroll.constant public static final int TYPE_WAVE = 2;
+	@Kroll.constant public static final int TYPE_WANDERINGCUBES = 3;
+	@Kroll.constant public static final int TYPE_PULSE = 4;
+	@Kroll.constant public static final int TYPE_CHASINGDOTS = 5;
+	@Kroll.constant public static final int TYPE_THREEBOUNCE = 6;
+	@Kroll.constant public static final int TYPE_CIRCLE = 7;
+	@Kroll.constant public static final int TYPE_CUBEGRID = 8;
+	@Kroll.constant public static final int TYPE_FADINGCIRCLE = 9;
+	@Kroll.constant public static final int TYPE_FOLDINGCUBE = 10;
+	@Kroll.constant public static final int TYPE_ROTATINGCIRCLE = 11;
+	
 	public SpinKitModule() {
 		super();
 	}
+
 	@Kroll.onAppCreate
 	public static void onAppCreate(TiApplication app) {
 		Log.d(LCAT, "inside onAppCreate");
